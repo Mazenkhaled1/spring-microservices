753d0cdb6d2c7e81a7bd28baac59802f02d30ea9
mazen khaled <khaledmazen64@gmail.com>
2026-07-04 02:03:31 +0300

    create dtos package and its classes and mapper class

---
+++ section2/loans/src/main/java/com/eazybytes/loans/service/impl/LoansServcieImpl.java
@@ -0,0 +1,10 @@
+//package com.eazybytes.loans.service.impl;
+//
+//import com.eazybytes.loans.service.ILoansService;
+//
+//public class LoansServcieImpl implements ILoansService {
+//    @Override
+//    public void createLoan(String mobileNumber) {
+//
+//    }
+//}
