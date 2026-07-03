902bcb46ddd0b5936e12650f3575d5f2eb16111e
mazen khaled <khaledmazen64@gmail.com>
2026-07-03 03:55:04 +0300

    enhance documentation of openApi by using @OpenAPIDefinition

--- section2/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,5 +1,10 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +12,26 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Mazen Khaled",
+						email = "mazenkhaled.jr@gmail.com",
+						url = "https://www.linkedin.com/in/mazen-khaled-mostafa/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.linkedin.com/in/mazen-khaled-mostafa/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation",
+				url = "http://localhost:8080/swagger-ui/index.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
