04582e0ed6cf5466e27e46d4d915f3d8ce8721c8
mazen khaled <khaledmazen64@gmail.com>
2026-07-16 11:23:01 +0300

    generate eazybakn correlationId in microservices

--- section9/accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomersServiceImpl.java
+++ section9/accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomersServiceImpl.java
@@ -29,24 +29,25 @@ public class CustomersServiceImpl implements ICustomersService {
 
     /**
      * @param mobileNumber - Input Mobile Number
+     *  @param correlationId - Correlation ID value generated at Edge server
      * @return Customer Details based on a given mobileNumber
      */
     @Override
-    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
-        var customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
                 () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
         );
-        var accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
                 () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
         );
 
         CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
         customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
 
-        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
         customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
 
-        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId, mobileNumber);
         customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
 
         return customerDetailsDto;
