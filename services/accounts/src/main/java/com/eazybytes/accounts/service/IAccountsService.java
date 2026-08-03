5f9b938e47b2dcd48f1d9fc8c576e569fc48e6da
mazen khaled <khaledmazen64@gmail.com>
2026-08-04 00:22:11 +0300

    refactor: restructure repo to professional layout

---
+++ services/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -0,0 +1,23 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    /*
+    *
+     @Param customerDto - CustomerDto Object
+     */
+
+
+     void createAccount(CustomerDto customerDto);
+
+     CustomerDto fetchAccount(String accountNumber);
+
+     boolean updateAccount(CustomerDto customerDto);
+
+     boolean deleteAccount(String accountNumber);
+
+     boolean updateCommunicationStatus(Long accountNumber);
+
+}
